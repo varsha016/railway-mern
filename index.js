76484d34da096b3a2c85d50f5d5d250c0545f396
varsha016 <103167478+varsha016@users.noreply.github.com>
2023-03-29 15:39:59 +0530

    inital

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(cors({
             "http://localhost:3000",
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://railway-mern-production-8dd2.up.railway.app"
 
 
         ]
